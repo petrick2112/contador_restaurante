4d8a3bd2effc21b30a8df88894551186d65cde74
petri <petrick2112@outlook.com>
2021-03-07 00:21:01 -0300

    Conclusão do app

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(MaterialApp(
       title: "Contador",
+      home: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
 
-      home: Home() ));
-}
+          Text(
+            "Pessoas 0",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
 
-  class Home extends StatefulWidget {
-    @override
-    _HomeState createState() => _HomeState();
-  }
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
 
-
-class _HomeState extends State<Home> {
-
-  int _pessoas = 0;
-  String _informa;
-
-  void _alterarPessoa(int valor){
-    setState(() {
-
-      _pessoas = _pessoas + valor;
-
-            if (_pessoas < 10) {
-              _informa = "Pode Entrar!";
-            }
-              else {
-              _informa = "O estabelecimento está cheio!";
-              _pessoas = 10;
-            }
-              
-            if (_pessoas <= 0) {
-              _pessoas = 0;
-              _informa = "O estabelecimento está vazio. Pode entrar!";
-            }
-        }
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Stack(
-
-      children: <Widget>[
-
-        Image.asset("images/restaurant.jpg",
-          fit: BoxFit.cover,
-          height: 1000.0,
-        ),
-
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-
-          children: <Widget>[
-
-            Text("Pessoas: $_pessoas",
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  decoration: TextDecoration.none
-              ),
-            ),
-
-            Row(mainAxisAlignment: MainAxisAlignment.center,
-
-              children: <Widget>[
-
-                TextButton(
-                  child: Text("+1",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                    ),
+              TextButton(
+                child: Text(
+                  "+1",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
                   ),
-                  onPressed: () {
-                    _alterarPessoa(1);
-                  },
                 ),
-
-                TextButton(
-                  child: Text("-1",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                    ),
-                  ),
-                  onPressed: () {
-                    _alterarPessoa(-1);
-                  },
-                ),
-
-              ],
-            ),
-
-            Text(_informa,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontStyle: FontStyle.italic,
-                  fontSize: 30,
-                  decoration: TextDecoration.none
+                onPressed: () {},
               ),
-            ),
-          ],
-        )
 
-      ],
-    );
-  }
+              TextButton(
+                child: Text(
+                  "-1",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+
+          Text(
+            "Pode Entrar!",
+            style: TextStyle(
+                color: Colors.white, fontStyle: FontStyle.italic, fontSize: 30),
+          ),
+        ],
+      )));
 }
-
